7b468b6bfbe8579c9454384e7fb9256b73902530
haider <haider2010c@aptechgdn.net>
2022-12-19 15:40:27 +0500

    project

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,18 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Hello/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
+import Hello from './components/Hello.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Hello
   }
 }
 </script>
---
+++ src/components/Hello.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>hello component</h1>
+</template>
+<script>
+export default{
+    name:"app",
+    components: {
+        helloworld
+    }
+}
+</script>
